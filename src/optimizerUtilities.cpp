608200bcb95b4656c8b82a415b38f82b544bb35d
James Fritz <54446032+James-Fritz@users.noreply.github.com>
2021-11-27 12:29:48 -0700

    Update optimizerUtilities.cpp

--- src/optimizerUtilities.cpp
+++ src/optimizerUtilities.cpp
@@ -23,6 +23,23 @@ std::string returnExpr(AST* mathExpr1, AST* mathExpr2, std::string op){
     }
 }
 
-void expressionElimination(Table* root) {
-    root. 
+void redundantFunctionElimination(Table* root) {
+    //todo 
+    // 1. implement traversal to find functions that do not have 'uses'
+    // 2. add entry name list of labels
+    // 3. open and remove redundant code from the ircode.asm file
+    // 3a. remove code from start of label to start of next label. This will remove redundant code
+
+    std::vector<String> labels;
+    
+    //Inside traversal code or for loop
+    if (e->uses > 0) {
+        labels.push_back(e->name);
+    }
+    //end for loop
+
+    IrGen.openFile();
+    // somehow traverse file
+    // delete entries between two points, 
+    IrGen.closeFile();
 }
